69dc964d118cef0d475347053d32e9825412be01
Dushan <dushanz@gmail.com>
2024-06-19 23:42:56 +0800

    Initial commit

---
+++ src/test/java/com/dushanz/bookmanager/BookManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dushanz.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
